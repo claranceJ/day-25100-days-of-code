2729eff8071a8aa553f59a9ae51f320bf33db41c
claranceJ <77950647+claranceJ@users.noreply.github.com>
2023-04-21 15:49:51 +0000

    improving my skills in subroutine and multiple arguments

---
+++ subroutine.py
@@ -0,0 +1,8 @@
+def add_numbers(num1,num2):
+  result=num1+num2
+  print("the sum of the two numbers is", result )
+
+number_1=int(input("Enter number: "))
+number_2=int(input("Enter second number: "))
+
+add_numbers(number_1,number_2)
